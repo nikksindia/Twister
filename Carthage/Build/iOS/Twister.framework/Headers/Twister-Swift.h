bc18c4b06168dfe6150e26b4fb9bb7db8b62a121
Nikhil Sharma <nisharma@dminc.com>
2018-10-23 17:22:20 +0530

    cathrage version updated to 2.0

--- Carthage/Build/iOS/Twister.framework/Headers/Twister-Swift.h
+++ Carthage/Build/iOS/Twister.framework/Headers/Twister-Swift.h
@@ -172,7 +172,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # define SWIFT_DEPRECATED_OBJC(Msg) SWIFT_DEPRECATED_MSG(Msg)
 #endif
 #if __has_feature(modules)
-@import ObjectiveC;
 #endif
 
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
@@ -184,12 +183,5 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #pragma clang diagnostic ignored "-Wnullability"
 
 SWIFT_MODULE_NAMESPACE_PUSH("Twister")
-
-SWIFT_CLASS("_TtC7Twister7Twister")
-@interface Twister : NSObject
-/// Initializer
-- (nonnull instancetype)init SWIFT_UNAVAILABLE;
-@end
-
 SWIFT_MODULE_NAMESPACE_POP
 #pragma clang diagnostic pop
