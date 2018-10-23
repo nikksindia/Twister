25abd49fc145aa91f0a89e723a260a1297b41a06
Nikhil Sharma <nisharma@dminc.com>
2018-10-23 16:00:14 +0530

    chnaged pod version to 2.0

---
+++ Twister/TwistEncoder.swift
@@ -0,0 +1,9 @@
+//
+//  TwistEncoder.swift
+//  Twister
+//
+//  Created by Nikhil Sharma on 23/10/18.
+//  Copyright © 2018 Nikhil Sharma. All rights reserved.
+//
+
+import Foundation
---
+++ Twister/TwistResponse.swift
@@ -0,0 +1,9 @@
+//
+//  TwistResponse.swift
+//  Twister
+//
+//  Created by Nikhil Sharma on 23/10/18.
+//  Copyright © 2018 Nikhil Sharma. All rights reserved.
+//
+
+import Foundation
